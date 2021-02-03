a0cb38ea7fae63ebcb2d4d74438b9d170f2e5ff9
una.sybit.de <mma@sybit.de>
2021-02-03 20:02:49 +0000

    set JAVA_HOME for installation

--- install.sh
+++ install.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+export JAVA_HOME=/opt/jdk/sapmachine
+
+export PATH=$JAVA_HOME/bin:$PATH
+
 commerceVersion=$1
 
-cd /opt/commerce/vanillacommerce
+echo "current JAVA_HOME is: " $JAVA_HOME
+echo "commerce version which should be installed: ${commerceVersion}"
+
 ./gradlew install -P hybrisVersion=${commerceVersion}
