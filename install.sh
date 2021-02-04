5bb3e45c42956a2dd0e28e3b84028611088f9c51
una.sybit.de <mma@sybit.de>
2021-02-04 10:42:08 +0000

    stop and start hybris server via install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 export JAVA_HOME=/opt/jdk/sapmachine
-
 export PATH=$JAVA_HOME/bin:$PATH
 
 commerceVersion=$1
@@ -9,4 +8,13 @@ commerceVersion=$1
 echo "current JAVA_HOME is: " $JAVA_HOME
 echo "commerce version which should be installed: ${commerceVersion}"
 
+echo "stopping hybris server"
+./hybrisserver.sh stop
+
+sleep 10
+
+echo "starting installation and initialization"
 ./gradlew install -P hybrisVersion=${commerceVersion}
+
+echo "starting hybris server"
+./hybrisserver.sh start
